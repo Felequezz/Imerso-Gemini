b7ac38463a01fe9e3a43940404b3ab02a332fffb
Mateus Henrique <165807607+Felequezz@users.noreply.github.com>
2024-09-07 16:33:18 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,13 @@
+let dados = [ {
+  titulo : "raças",
+  descrição : "as raças que definem as caracteristicas fisicas e as habilidades que o personagem irá possuir. Em nosso servidor há 11 opções, sendo estes: Kitsune; Anão; Humano; Elfo; Minotauro; Feral; Succubus; Tritâo; Metamorfo; Fada; Maldição.",
+  tags: "raças, caracteristicas, habilidades, corpo, fisico, inicial, poderes, humano, kitsune, elfo, anão, feral, maldição, succubus, fada, iniciais"
+}, {  
+  titulo : "classes",
+  descrição : "As classes são oque define as capacidades e limitações do seu personagem, além da raça. Em nosso servidor você pode escolher entre: Xamã; Atirador; Berserker; Lutador; Artificie; Bardo; Ladino. Cada classe tem seu equipamento inicial que é dado gratuitamente pela guilda, para auxiliar o início da sua jornada.",
+  tags: "equipamento, inicial, escolha, escolher, espadachim, mago, ladino, paladino, bardo`, berserker, iniciais"
+}, {
+  titulo : "itens",
+  descrição : "os itens são des de armas poderosíssimas a consumíveis muito úteis. Cada personagem obtem um no início de sua jornada, e durante, em missões. É importante notar que tem limite de itens carregaveis (segue as leis do sistema de inventário).",
+  tags: "arma, armas, inicial, espada, lança, escudo, armadura, equipamento, inventario, inventário, itens, poção, poções, iniciais"
+} ];
